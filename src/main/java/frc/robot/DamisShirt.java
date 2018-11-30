19bba8fda0523d4a61838a9993d0edca2f053f13
Mr. J.A.Bianchi <MisterBianchi@gmail.com>
2018-11-30 16:45:29 -0500

    added DamisShirt -JAB

---
+++ src/main/java/frc/robot/DamisShirt.java
@@ -0,0 +1,12 @@
+public class DamisShirt {
+
+    public static int numberOnShirt = 7;
+
+    public static void main (String[] avocado){
+
+        System.out.print("I like Dami's " + numberOnShirt + " red shirts");
+
+
+    }
+
+}
