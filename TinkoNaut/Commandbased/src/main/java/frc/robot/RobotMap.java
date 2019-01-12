cc2b0b881c6eb13a5b55e30361a728615a03d411
Mr. J.A.Bianchi <MisterBianchi@gmail.com>
2019-01-12 15:43:48 -0500

    Merged mordenis changes

--- TinkoNaut/Commandbased/src/main/java/frc/robot/RobotMap.java
+++ TinkoNaut/Commandbased/src/main/java/frc/robot/RobotMap.java
@@ -23,4 +23,97 @@ public class RobotMap {
   // number and the module. For example you with a rangefinder:
   // public static int rangefinderPort = 1;
   // public static int rangefinderModule = 1;
+
+
+    // Xbox controller button mappings
+    public final static int A_BUTTON = 1;
+    public final static int B_BUTTON = 2;
+    public final static int X_BUTTON = 3;
+    public final static int Y_BUTTON = 4;
+    public final static int LB_BUTTON = 5;
+    public final static int RB_BUTTON = 6;
+    public final static int BACK_BUTTON = 7;
+    public final static int START_BUTTON = 8;
+    public final static int LEFT_JOY_BUTTON = 9;
+    public final static int RIGHT_JOY_BUTTON = 10;
+    public final static int LEFT_X_AXIS = 0;
+    public final static int LEFT_Y_AXIS = 1;
+    public final static int LT_AXIS = 2;
+    public final static int RT_AXIS = 3;
+    public final static int RIGHT_X_AXIS = 4;
+    public final static int RIGHT_Y_AXIS = 5;
+    public final static int POV_UP = 0;
+    public final static int POV_UP_RIGHT = 45;
+    public final static int POV_RIGHT = 90;
+    public final static int POV_DOWN_RIGHT = 135;
+    public final static int POV_DOWN = 180;
+    public final static int POV_DOWN_LEFT = 225;
+    public final static int POV_LEFT = 270;
+    public final static int POV_UP_LEFT = 315;
+      
+    // Joystick ports
+    public final static int DRIVER_JOYSTICK_PORT = 0;
+    public final static int MANIPULATOR_JOYSTICK_PORT = 1;
+    
+    //Drive Joystick
+    public final static int FORWARD_AXIS = RIGHT_Y_AXIS;
+    public final static int STRAFE_AXIS = RIGHT_X_AXIS;
+    public final static int TURN_AXIS = LEFT_X_AXIS;
+    public final static int MOVING_Y_AXIS = RIGHT_Y_AXIS;
+    public final static int MOVING_X_AXIS = RIGHT_X_AXIS;
+    public final static int HEADING_Y_AXIS = LEFT_Y_AXIS;
+    public final static int HEADING_X_AXIS = LEFT_X_AXIS;
+    public final static int FIELD_DRIVING_FLAG_ON_BUTTON = RB_BUTTON;
+    public final static int FIELD_DRIVING_FLAG_OFF_BUTTON = LB_BUTTON;
+    public final static int YAW_ZERO_BUTTON = START_BUTTON;
+    public final static int FACE_BACKWARD_POV = POV_DOWN;
+    public final static int FACE_LEFT_POV = POV_LEFT;
+    public final static int FACE_RIGHT_POV = POV_RIGHT;
+    public final static int FACE_FORWARD_POV = POV_UP;
+    
+    //Mani Joystick
+    public final static int LIFT_AXIS = LEFT_Y_AXIS;	
+    public final static int EAT_BUTTON = A_BUTTON;
+    public final static int SPIT_BUTTON = B_BUTTON;
+    public final static int ZERO_ENCODER_BUTTON = BACK_BUTTON;
+    public final static int RELAX_MOTOR_BUTTON = START_BUTTON;
+    public final static int LIFT_BOTTOM_HEIGHT_POV = POV_DOWN;
+    public final static int LIFT_EXCHANGE_HEIGHT_POV = POV_LEFT;
+    public final static int LIFT_TIER2_HEIGHT_POV = POV_RIGHT;
+    public final static int LIFT_SWITCH_HEIGHT_POV = POV_UP;
+    public final static int REACH_BUTTON =  Y_BUTTON;
+    public final static int PULL_UP_BUTTON =  X_BUTTON;
+    public final static int FLIP_AXIS = RT_AXIS;
+    public final static int DIP_AXIS = LT_AXIS;
+    public final static int LOCK_BUTTON = 75;
+    public final static int LIFT_TOP_HEIGHT = LB_BUTTON;
+
+    //public final static int COMPRESSOR_BUTTON = LB_BUTTON;
+    public final static int SCALE_HEIGHT_AXIS = RIGHT_Y_AXIS;
+    public final static int PRESSURE_OVERRIDE_BUTTON = RB_BUTTON;
+  
+    // Drive train motor channels
+    public final static int DRIVE_FRONT_LEFT_CHANNEL = 3;
+    public final static int DRIVE_BACK_LEFT_CHANNEL = 4;
+    public final static int DRIVE_FRONT_RIGHT_CHANNEL = 2;
+      public final static int DRIVE_BACK_RIGHT_CHANNEL = 1;
+      
+    // Mouth motor channels
+    public final static int MOUTH_RIGHT_CHANNEL = 6;
+    public final static int MOUTH_LEFT_CHANNEL = 5;
+    public final static int MOUTH_LIMITER_CHANNEL = 0;
+  
+    // Lift motor channel
+    public final static int LIFT_MOTOR_CHANNEL = 7;
+    public final static int LIFT_LIMIT_TOP_CHANNEL = 1;
+      
+    //NavX angles
+    public final static double LEFT_WALL_ANGLE = -90.0; //270.0?
+    public final static double RIGHT_WALL_ANGLE = 90.0;
+    public final static double BACK_WALL_ANGLE = 179.9;
+    public final static double FRONT_WALL_ANGLE = 0.0;
+  
+  
+
+
 }
--- TinkoNaut/Commandbased/src/main/java/frc/robot/subsystems/HKdrive.java
+++ TinkoNaut/Commandbased/src/main/java/frc/robot/subsystems/HKdrive.java
@@ -1,5 +1,13 @@
 package org.usfirst.frc.team1660.robot;
 
+/*
+ * Harlem Knights Drive class for:
+ * 		+Mecanum Wheel drive
+ * 		+Using TalsonSRX speed controllers
+ * 		+With angles determined by NavX sensor
+ * online repository: www.github.com/hk1660/frc2018
+ */
+
 /*-----IMPORTED LIBRARIES-----*/
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -14,9 +22,12 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.SPI;
 
-   public class HKDrive implements PIDOutput {
+public class HKDrive implements PIDOutput {
 
-    private WPI_TalonSRX frontLeft;
+	/*----DECLARED GLOBAL VARIABLES-------*/
+
+	//Drivetrain Declarations
+	private WPI_TalonSRX frontLeft;
 	private WPI_TalonSRX backLeft;
 	private WPI_TalonSRX frontRight;
 	private WPI_TalonSRX backRight;
@@ -32,8 +43,31 @@ import edu.wpi.first.wpilibj.SPI;
 	static final double kF = 0.00;
 	static final double kToleranceDegrees = 2.0;
 
+	//angle variables
+	double offsetAngle = 0.0;
+	double roboAngle = 0.0;
+	double lastUsedAngle;
+	boolean autoTurnFlag = false;	//automatic driving
+	boolean fieldDrivingFlag = false; //is using field-oriented driving
+	double rotateToAngleRate;
 
-    public void driveInit() {
+	//drive parameters
+	double strafeParameter = 0.0;
+	double forwardParameter = 0.0;
+	double turnParameter = 0.0;
+	double angleParameter = 0.0;
+
+
+	//Joystick fields
+	private Joystick driverStick;
+
+	public HKDrive(Joystick joy){
+		this.driverStick = joy;
+	}
+
+	//HKDrive init method
+	@SuppressWarnings("deprecation")
+	public void driveInit() {
 
 		//Drivetrain Initializations
 		frontLeft = new WPI_TalonSRX(RobotMap.DRIVE_FRONT_LEFT_CHANNEL);
@@ -43,6 +77,301 @@ import edu.wpi.first.wpilibj.SPI;
 
 		//we think the constructor switched the 3rd & 4th parameters
 		mecDrive = new MecanumDrive(frontLeft, backLeft, frontRight, backRight);
-    }
-}
 
+		//navx intialization
+		try {
+			navx = new AHRS(SPI.Port.kMXP); //navX-MXP initialized with (SPI, I2C, TTL UART) and USB //http://navx-mxp.kauailabs.com/guidance/selecting-an-interface.
+		} catch (RuntimeException ex ) {
+			DriverStation.reportError("Error instantiating navX MXP:  " + ex.getMessage(), true);
+		}
+
+		//PID control initialization
+		turnController = new PIDController(kP, kI, kD, kF, navx, this);
+		turnController.setInputRange(-180.0f,  180.0f);
+		turnController.setOutputRange(-1.0, 1.0);
+		turnController.setAbsoluteTolerance(kToleranceDegrees);
+		turnController.setContinuous(true);
+
+		/* Add the PID Controller to the Test-mode dashboard, allowing manual  */
+		/* tuning of the Turn Controller's P, I and D coefficients.            */
+		/* Typically, only the P value needs to be modified.                   */
+		LiveWindow.addActuator("DriveSystem", "RotateController", turnController);
+
+		//PDP init
+		pdp = new PowerDistributionPanel(RobotMap.PDP_ID);
+
+		this.yawZeroing();
+
+	}
+
+
+	public void drive() {
+		mecDrive.driveCartesian(strafeParameter, forwardParameter, turnParameter, angleParameter);
+
+		//Prints
+		SmartDashboard.putNumber("forwardParam",	forwardParameter);
+		SmartDashboard.putNumber("strafeParam",	strafeParameter);
+		SmartDashboard.putNumber("turnParam",	turnParameter);
+		SmartDashboard.putNumber("angleParam",	angleParameter);
+		SmartDashboard.putBoolean("AutoTurning?", autoTurnFlag);
+		
+		SmartDashboard.putNumber("speed control frontLeftCurrent", this.frontLeft.getOutputCurrent());
+		SmartDashboard.putNumber("backLeftCurrent", this.backLeft.getOutputCurrent());
+		SmartDashboard.putNumber("frontRightCurrent", this.frontRight.getOutputCurrent());
+		SmartDashboard.putNumber("backRightCurrent", this.backRight.getOutputCurrent());
+
+		SmartDashboard.putNumber("pdp backRightCurrent", pdp.getCurrent(0));
+		SmartDashboard.putNumber("pdp frontRightCurrent", pdp.getCurrent(1));
+		SmartDashboard.putNumber("pdp backLeftCurrent", pdp.getCurrent(14));
+		SmartDashboard.putNumber("pdp frontLeftCurrent", pdp.getCurrent(13));
+		SmartDashboard.putNumber("Lift Current", pdp.getCurrent(15));
+		
+		
+		
+
+		
+	}
+
+	//method to drive straight in AUTO
+	public void autoDriveStraight(double fwdSpeed){
+		strafeParameter = 0.0;
+		forwardParameter = fwdSpeed;
+		turnParameter = 0.0;
+		angleParameter = navx.getAngle();
+
+		drive();
+	}
+
+	//method to check joysticks for driving the robot -Nana B & Matthew W
+	public void checkDriving() {
+
+		double strafeJoy = squareIt(driverStick.getRawAxis(RobotMap.STRAFE_AXIS)) ; // right and left on the left thumb stick?
+		double forwardJoy = -squareIt(driverStick.getRawAxis(RobotMap.FORWARD_AXIS));// up and down on left thumb stick?
+		double turnJoy = squareIt(driverStick.getRawAxis(RobotMap.TURN_AXIS));// right and left on right thumb stick
+
+		if(autoTurnFlag == false && fieldDrivingFlag){
+
+			strafeParameter = strafeJoy;
+			forwardParameter = forwardJoy;
+			turnParameter = turnJoy;
+			angleParameter = getCurrentAngle();
+		}	
+		else if (autoTurnFlag == false && fieldDrivingFlag == false){
+			strafeParameter = strafeJoy;
+			forwardParameter = forwardJoy;
+			turnParameter = turnJoy;
+			angleParameter = 0.0;
+		}
+		else if (autoTurnFlag == true) {
+			strafeParameter = strafeJoy;
+			forwardParameter = forwardJoy;
+			turnParameter = this.rotateToAngleRate;
+			angleParameter = getCurrentAngle();
+		}
+
+		drive();
+
+	}
+
+
+	//method to square the joystick values to provide less sensitivity for small movements -Matthew W
+	public double squareIt(double joy) {
+
+		double squared = joy * joy;
+		if(joy < 0) { squared *= -1; }
+		return squared;
+	}
+
+	public void checkFieldFlag() {
+		if(driverStick.getRawButton(RobotMap.FIELD_DRIVING_FLAG_ON_BUTTON)) {
+			fieldDrivingFlag = true;
+		}
+		if(driverStick.getRawButton(RobotMap.FIELD_DRIVING_FLAG_OFF_BUTTON)) {
+			fieldDrivingFlag = false;	
+		}
+		SmartDashboard.putBoolean("FieldDriveFlag?", fieldDrivingFlag);
+	}
+
+
+	// method to turn robot to different angles automatically @aldenis @marlahna
+	public void checkAutoTurn(){
+
+		if(driverStick.getPOV()==RobotMap.FACE_LEFT_POV){
+			autoTurn(RobotMap.LEFT_WALL_ANGLE);						//aiming to the LEFT
+		}
+		else if(driverStick.getPOV()==RobotMap.FACE_BACKWARD_POV){
+			autoTurn(RobotMap.BACK_WALL_ANGLE);						//heading back towards driverStation
+		}
+		else if(driverStick.getPOV()==RobotMap.FACE_RIGHT_POV){
+			autoTurn(RobotMap.RIGHT_WALL_ANGLE);					//aiming to the RIGHT
+		}
+		else if(driverStick.getPOV()==RobotMap.FACE_FORWARD_POV){
+			autoTurn(RobotMap.FRONT_WALL_ANGLE);					//heading away from driverStation
+		}
+		else{
+			autoTurnFlag=false;
+		}
+	}
+
+	public void autoTurn(double futureAngle){
+		autoTurnFlag = true;
+		turnController.enable();
+		turnController.setSetpoint(futureAngle);
+		turnParameter = rotateToAngleRate;
+
+		//strafeParameter = 0.0;
+		//forwardParameter = 0.0;
+		//angleParameter = 0.0;
+
+		drive();
+
+		SmartDashboard.putNumber("LastAutoAngle", futureAngle);
+	}
+
+	//joystick method to manually resets the robot to the field's orientation -Aldenis G
+	public void checkYawZero(){
+
+		if (driverStick.getRawButton(RobotMap.YAW_ZERO_BUTTON)) {
+			yawZeroing();
+			//setOffsetAngle();
+		}
+	}
+
+	public void setOffsetAngle() {
+		offsetAngle = navx.getAngle();
+	}
+
+
+	//method to update the angle the robot is facing on the field -Aldenis G
+	public int getCurrentAngle(){
+
+		int rawAngle = Math.floorMod((int)navx.getAngle(), 360);
+		offsetAngle = Math.floorMod((int)offsetAngle, 360);
+		int fieldAngle = Math.floorMod((rawAngle -(int) offsetAngle), 360);
+
+		SmartDashboard.putNumber("rawAngle", rawAngle);
+		SmartDashboard.putNumber("offsetAngle", offsetAngle);
+		SmartDashboard.putNumber("fieldAngle", fieldAngle);
+
+		return fieldAngle;
+	}
+
+
+	public void navxReset() {
+		navx.reset();
+	}
+
+	public void goForwardPercentOutput(double speed){
+		//this.mecDrive.driveCartesian(0.0, speed, 0.0, navx.getAngle());
+
+	} 
+
+
+	//auto method to drive at a constant voltage
+	public void goForwardVoltage(double desiredVoltage){
+
+		double mVoltage = pdp.getVoltage();
+		double desiredPercentForwardSpeed = desiredVoltage / mVoltage;
+
+		//mecDrive.driveCartesian(0.0, desiredPercentForwardSpeed, 0.0, navx.getAngle()); 
+		//mecDrive.driveCartesian(0.0, desiredPercentForwardSpeed, 0.0, 0.0); 
+
+		strafeParameter = 0.0;
+		forwardParameter = desiredPercentForwardSpeed;
+		turnParameter = 0.0;
+		angleParameter = 0.0;
+
+		drive();
+
+	}
+
+
+	//auto method to drive at a constant voltage WHILE rotating to desired angle
+	public void goStraightAccurate(double desiredVoltage, double angleFacing){
+
+		double mVoltage = pdp.getVoltage();
+		double desiredPercentForwardSpeed = desiredVoltage / mVoltage;
+
+		autoTurnFlag = true;
+		turnController.enable();
+		turnController.setSetpoint(angleFacing);
+		turnParameter = rotateToAngleRate;
+
+		strafeParameter = 0.0;
+		forwardParameter = desiredPercentForwardSpeed;
+		angleParameter = 0.0;
+
+		drive();
+
+	}
+
+	//auto method to drive at a constant voltage WHILE rotating to desired angle
+	public void goDiagonal(double desiredVoltage, double angleFacing, double fwdpct){
+
+		double mVoltage = pdp.getVoltage();
+		double desiredSpeed = desiredVoltage / mVoltage;
+
+		autoTurnFlag = true;
+		turnController.enable();
+		turnController.setSetpoint(angleFacing);
+		turnParameter = rotateToAngleRate;
+
+		//turnParameter = 0.0;
+		//strafeParameter = Math.sin( Math.toRadians( desiredSpeed ) );
+		//forwardParameter =Math.cos( Math.toRadians( desiredSpeed ) );
+		
+		strafeParameter  = desiredSpeed;
+		forwardParameter = desiredSpeed * fwdpct;		//need to scale down bc FWD with mec wheels is mechanically much easier than STRAFING
+		angleParameter = 0.0;
+
+		if(fwdpct < 0) {
+			strafeParameter *= -1;
+		}	
+		
+		drive();
+
+	}
+
+	
+	//auto method to turn at a constant voltage
+	public void turnVoltage(double desiredVoltage){
+
+		double mVoltage = pdp.getVoltage();
+		double desiredPercentTurnSpeed = desiredVoltage / mVoltage;
+
+		turnParameter = desiredPercentTurnSpeed;
+		strafeParameter = 0.0;
+		forwardParameter = 0.0;
+		angleParameter = 0.0;
+		drive();
+
+		//mecDrive.driveCartesian(0.0, 0.0, desiredPercentTurnSpeed, 0.0); 
+
+	}
+
+
+
+	public void stop(){
+
+		turnParameter = 0.0;
+		strafeParameter = 0.0;
+		forwardParameter = 0.0;
+		angleParameter = 0.0;
+		drive();
+	}
+
+	@Override
+	/* This function is invoked periodically by the PID Controller, */
+	/* based upon navX-MXP yaw angle input and PID Coefficients.    */
+	public void pidWrite(double output) {
+		rotateToAngleRate = output;
+	}
+
+	public void yawZeroing() {
+		navx.zeroYaw();
+
+	}
+
+
+
+}
