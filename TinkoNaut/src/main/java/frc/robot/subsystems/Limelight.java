7975fde1de5e10f667e796e63bfd2dfff641f506
Mr. J.A.Bianchi <MisterBianchi@gmail.com>
2019-01-24 13:02:05 -0500

    ll getUpdates method started -JAB & HS

--- TinkoNaut/src/main/java/frc/robot/subsystems/Limelight.java
+++ TinkoNaut/src/main/java/frc/robot/subsystems/Limelight.java
@@ -9,22 +9,28 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Limelight{
 
-    // NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-    // NetworkTableEntry tx = table.getEntry("tx");
-    // NetworkTableEntry ty = table.getEntry("ty");
-    // NetworkTableEntry ta = table.getEntry("ta");
 
-    // //read values periodically
-    // double x = tx.getDouble(0.0);
-    // double y = ty.getDouble(0.0);
-    // double area = ta.getDouble(0.0);
-
-    // //post to smart dashboard periodically
-    // SmartDashboard.putNumber("LimelightX", x);
-    // SmartDashboard.putNumber("LimelightY", y);
-    // SmartDashboard.putNumber("LimelightArea", area);
 
     public Limelight(){
+
+    }
+
+    public void getUpdates(){
+        
+        // NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+        // NetworkTableEntry tx = table.getEntry("tx");
+        // NetworkTableEntry ty = table.getEntry("ty");
+        // NetworkTableEntry ta = table.getEntry("ta");
+
+        // //read values periodically
+        // double x = tx.getDouble(0.0);
+        // double y = ty.getDouble(0.0);
+        // double area = ta.getDouble(0.0);
+
+        // //post to smart dashboard periodically
+        // SmartDashboard.putNumber("LimelightX", x);
+        // SmartDashboard.putNumber("LimelightY", y);
+        // SmartDashboard.putNumber("LimelightArea", area);
 
     }
 
