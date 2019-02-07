e2b2d9f36cb2ba81aaf37261c42df04412a272b5
KT0122 <khaliltavarez@gmail.com>
2019-02-07 16:11:26 -0500

    ElevatorWInchPID

--- TinkoNaut/src/main/java/frc/robot/subsystems/ElevatorWinchPID.java
+++ TinkoNaut/src/main/java/frc/robot/subsystems/ElevatorWinchPID.java
@@ -7,16 +7,17 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Victor;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import frc.robot.Robot;
 import frc.robot.RobotMap;
-
 
 /**
  * The elevator subsystem uses PID to go to a given height. Unfortunately, in
@@ -24,82 +25,66 @@ import frc.robot.RobotMap;
  * world do to minor differences.
  */
 public class ElevatorWinchPID extends PIDSubsystem {
-  private final Victor m_motor;
-  private final AnalogPotentiometer m_pot;
+  private final WPI_TalonSRX winchMotor;
+  private final int encoder;
   private DoubleSolenoid theLocker;
 
-  private static final double kP_real = 4;
-  private static final double kI_real = 0.07;
-  private static final double kP_simulation = 18;
-  private static final double kI_simulation = 0.2;
+  // private static final double kP_real = 4;
+  // private static final double kI_real = 0.07;
+  // private static final double kP_simulation = 18;
+  // private static final double kI_simulation = 0.2;
 
   /**
    * Create a new elevator subsystem.
    */
   public ElevatorWinchPID() {
-    super(kP_real, kI_real, 0);
-    if (Robot.isSimulation()) { // Check for simulation and update PID values
-      getPIDController().setPID(kP_simulation, kI_simulation, 0, 0);
-    }
-    setAbsoluteTolerance(0.005);
+    super("Winch", 2.0, 0.0, 0.0);// The constructor passes a name for the subsystem and the P, I and D constants
+                                  // that are used when computing the motor output
+    setAbsoluteTolerance(0.05);
+    getPIDController().setContinuous(false);
 
-    m_motor = new Victor(5);
+    winchMotor = new WPI_TalonSRX(RobotMap.WINCH_MOTOR_CHANNEL);
+    encoder = winchMotor.getSelectedSensorPosition(0);
+    theLocker = new DoubleSolenoid(RobotMap.PISTON_IN_WINCH_CHANNEL, RobotMap.PISTON_OUT_WINCH_CHANNEL);
 
-    // Conversion value of potentiometer varies between the real world and
-    // simulation
-    if (Robot.isReal()) {
-      m_pot = new AnalogPotentiometer(2, -2.0 / 5);
-    } else {
-      m_pot = new AnalogPotentiometer(2); // Defaults to meters
-    }
-    //Creates new double solonoid for locking and unlocking winch
-      theLocker = new DoubleSolenoid(RobotMap.PISTON_IN_WINCH_CHANNEL,RobotMap.PISTON_OUT_WINCH_CHANNEL);
-
-    // Let's name everything on the LiveWindow
-    addChild("Motor", m_motor);
-    addChild("Pot", m_pot);
   }
 
-  @Override
   public void initDefaultCommand() {
+  }
+
+  public double returnPIDInput() {
+    return encoder; // returns the sensor value that is providing the feedback for the system
+  }
+
+  protected void usePIDOutput(double output) {
+    winchMotor.pidWrite(output); // this is where the computed output value fromthe PIDController is applied to
+                                 // the motor
+  }
+
+  public void moveWinch(double speed) {
+    winchMotor.set(speed);
   }
 
   /**
    * The log method puts interesting information to the SmartDashboard.
    */
   public void log() {
-    SmartDashboard.putData("Elevator Pot", (AnalogPotentiometer) m_pot);
+    //SmartDashboard.putData("Elevator Pot", encoder);
   }
-  //Lock method will lock the winch's gearbox
-  public void lock(){
+
+  // Lock method will lock the winch's gearbox
+  public void lock() {
     theLocker.set(DoubleSolenoid.Value.kForward);
   }
 
-  //Unlock method does opposite of above method
-  public void unlock(){
-      theLocker.set(DoubleSolenoid.Value.kReverse);
+  // Unlock method does opposite of above method
+  public void unlock() {
+    theLocker.set(DoubleSolenoid.Value.kReverse);
   }
 
-  //Stops the solenoid
-  public void stop(){
+  // Stops the solenoid
+  public void stop() {
     theLocker.set(DoubleSolenoid.Value.kOff);
   }
 
-  /**
-   * Use the potentiometer as the PID sensor. This method is automatically
-   * called by the subsystem.
-   */
-  @Override
-  protected double returnPIDInput() {
-    return m_pot.get();
-  }
-
-  /**
-   * Use the motor as the PID output. This method is automatically called by
-   * the subsystem.
-   */
-  @Override
-  protected void usePIDOutput(double power) {
-    m_motor.set(power);
-  }
 }
