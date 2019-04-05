c100a4753cbd827a2f0f856f1edcf5b2f9e00a32
Haim Singh <haimsingh81@gmail.com>
2019-04-05 19:37:14 -0400

    Changed set heights

--- TinkoNaut/src/main/java/frc/robot/RobotMap.java
+++ TinkoNaut/src/main/java/frc/robot/RobotMap.java
@@ -108,10 +108,10 @@ public class RobotMap {
     //OG vals: Lvl 1 - 200.0, 1.5 - 24000.0, 2 - 57000.0, 2.5 - 82000.0, 3 - 109000.0, 3.5 - 135000.0
     public final static double LEVEL_0 = 0;
     public final static double LEVEL_1 = 5942.0;
-    public final static double LEVEL_1_5 = 49010.0;
-    public final static double LEVEL_2 = 88763.0;
-    public final static double EAT_CARGO_HEIGHT = 141622.0;
-    public final static double LEVEL_2_5 = 140138.0;
+    public final static double LEVEL_1_5 = 52000.0;
+    public final static double LEVEL_2 = 99840.0;
+    public final static double EAT_CARGO_HEIGHT = 123000.0;
+    public final static double LEVEL_2_5 = 160000.0;
     public final static double LEVEL_3 = 187462.0;
     public final static double LEVEL_3_5 = 263260.0;
 
